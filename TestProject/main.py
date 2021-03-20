b1daf4685a36e85ece7625f02e6baaaa833ff407
Nicholas Leite <nicklleite@gmail.com>
2021-03-19 22:17:28 -0300

    init

---
+++ TestProject/main.py
@@ -0,0 +1,17 @@
+# Section 02
+
+favorite_foods = [
+    'Strogonoff',
+    'Frango empanado',
+    'Risoto',
+    'Macarrão',
+    'Bolo de carne'
+]
+
+print(favorite_foods[3])
+
+favorite_foods.append('Torta de frango')
+
+favorite_foods.insert(3, 'Doguinho')
+
+print(favorite_foods)
